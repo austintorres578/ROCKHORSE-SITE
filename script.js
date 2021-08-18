5c13df7cc1834eeb410e83042065c5651b2b9503
austintorres578 <78386688+austintorres578@users.noreply.github.com>
2021-08-18 15:01:22 -0400

    Song List responsiveness
    added responsiveness to the song list

---
+++ script.js
@@ -0,0 +1,18 @@
+var seventiesList = document.getElementById("seventies-song-list");
+
+var eightiesList = document.getElementById("eighties-song-list");
+
+var ninetiesList = document.getElementById("nineties-song-list");
+
+var seventiesBtn = document.getElementById("seventies-btn");
+
+var eightiesBtn = document.getElementById("eighties-btn");
+
+var ninetiesBtn = document.getElementById("nineties-btn");
+
+addEventListener.seventiesBtn('click',toggleSeventiesList());
+
+addEventListener.eightiesBtn('click',toggleEightiesList());
+
+addEventListener.ninetiesBtn('click',toggleNinetiesList());
+
