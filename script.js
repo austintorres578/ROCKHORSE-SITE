516fab1beb83126e82a548f1e716bbfeebe967bc
austintorres578 <78386688+austintorres578@users.noreply.github.com>
2021-08-29 13:07:49 -0400

    add song list/video slider functionality

--- script.js
+++ script.js
@@ -1,18 +1,105 @@
-var seventiesList = document.getElementById("seventies-song-list");
+var videoOne = document.getElementById("video-1");
+var videoTwo = document.getElementById("video-2");
+var videoThree = document.getElementById("video-3");
 
-var eightiesList = document.getElementById("eighties-song-list");
+var videoOneBtns = document.getElementById("video-1-btns");
+var videoOneLeft = document.getElementById("video-1-left");
+var videoOneRight = document.getElementById("video-1-right");
 
-var ninetiesList = document.getElementById("nineties-song-list");
+var videoTwoBtns = document.getElementById("video-2-btns");
+var videoTwoLeft = document.getElementById("video-2-left");
+var videoTwoRight = document.getElementById("video-2-right");
+
+var videoThreeBtns = document.getElementById("video-3-btns");
+var videoThreeLeft = document.getElementById("video-3-left");
+var videoThreeRight = document.getElementById("video-3-right");
+
+videoOneLeft.addEventListener("click",changeVideoThreeFromOne);
+videoOneRight.addEventListener("click", changeVideoTwoFromOne);
+
+videoTwoLeft.addEventListener("click",changeVideoOneFromTwo);
+videoTwoRight.addEventListener("click",changeVideoThreeFromTwo);
+
+videoThreeLeft.addEventListener("click",changeVideoTwoFromThree);
+videoThreeRight.addEventListener("click",changeVideoOneFromThree);
+
+function changeVideoThreeFromOne(){
+    videoOne.style.display="none";
+    videoOneBtns.style.display="none";
+    videoThree.style.display="block";
+    videoThreeBtns.style.display="flex";
+};
+
+function changeVideoTwoFromOne(){
+    videoOne.style.display="none";
+    videoOneBtns.style.display="none";
+    videoTwo.style.display="block";
+    videoTwoBtns.style.display="flex";
+};
+
+function changeVideoOneFromTwo(){
+    videoTwo.style.display="none";
+    videoTwoBtns.style.display="none";
+    videoOne.style.display="block";
+    videoOneBtns.style.display="flex";
+};
+
+function changeVideoThreeFromTwo(){
+    videoTwo.style.display="none";
+    videoTwoBtns.style.display="none";
+    videoThree.style.display="block";
+    videoThreeBtns.style.display="flex";
+};
+
+function changeVideoTwoFromThree(){
+    videoThree.style.display="none";
+    videoThreeBtns.style.display="none";
+    videoTwo.style.display="block";
+    videoTwoBtns.style.display="flex";
+};
+
+function changeVideoOneFromThree(){
+    videoThree.style.display="none";
+    videoThreeBtns.style.display="none";
+    videoOne.style.display="block";
+    videoOneBtns.style.display="flex";
+};
 
 var seventiesBtn = document.getElementById("seventies-btn");
-
 var eightiesBtn = document.getElementById("eighties-btn");
-
 var ninetiesBtn = document.getElementById("nineties-btn");
 
-addEventListener.seventiesBtn('click',toggleSeventiesList());
+var seventiesSongList = document.getElementById("seventies-song-list");
+var eightiesSongList = document.getElementById("eighties-song-list");
+var ninetiesSongList = document.getElementById("nineties-song-list");
 
-addEventListener.eightiesBtn('click',toggleEightiesList());
+seventiesBtn.addEventListener("click",changeToSeven);
+eightiesBtn.addEventListener("click",changeToeight);
+ninetiesBtn.addEventListener("click",changeToNine);
 
-addEventListener.ninetiesBtn('click',toggleNinetiesList());
+function changeToSeven(){
+    seventiesBtn.style.backgroundColor="red";
+    seventiesSongList.style.display="block";
+    eightiesSongList.style.display="none";
+    eightiesBtn.style.backgroundColor="black";
+    ninetiesSongList.style.display="none";
+    ninetiesBtn.style.backgroundColor="black";
+};
 
+function changeToeight(){
+    eightiesSongList.style.display="block";
+    eightiesBtn.style.backgroundColor="red";
+    seventiesBtn.style.backgroundColor="black";
+    seventiesSongList.style.display="none";
+    ninetiesSongList.style.display="none";
+    ninetiesBtn.style.backgroundColor="black";
+};
+
+function changeToNine(){
+    ninetiesSongList.style.display="block";
+    ninetiesBtn.style.backgroundColor="red";
+    seventiesBtn.style.backgroundColor="black";
+    seventiesSongList.style.display="none";
+    eightiesSongList.style.display="none";
+    eightiesBtn.style.backgroundColor="black";
+};
